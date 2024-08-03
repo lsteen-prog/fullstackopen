c8e727cd918918e29b0fe1c59cc6f5bf4e73d8ab
lsteen-prog <linda.steenhuizen@gmail.com>
2024-08-03 15:12:01 +0200

    step 8

---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,14 @@
+const Total = ({ parts }) => {
+    console.log(parts)
+
+    const total = parts.reduce(
+        (sum, part) => sum + part['exercises'],
+        0
+    )
+    
+    return (     
+        <p>Number of exercises {total}</p>
+    )
+}
+
+export default Total
