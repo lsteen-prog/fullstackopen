63172f301d8533dcd4cd3ea8316ed4f59e127fbd
lsteen-prog <linda.steenhuizen@gmail.com>
2024-08-03 15:12:36 +0200

    step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,5 @@
 import Course from './components/Course'
 
-const Header = ({ course }) => <h1>{course}</h1>
-
-// const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const parts = [
-//     {
-//       name: 'Fundamentals of React',
-//       exercises: 10
-//     },
-//     {
-//       name: 'Using props to pass data',
-//       exercises: 7
-//     },
-//     {
-//       name: 'State of a component',
-//       exercises: 14
-//     }
-//   ]
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content parts={parts} />
-//       <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-//     </div>
-//   )
-// }
-
-// export default App
-
 const App = () => {
   const course = {
     id: 1,
