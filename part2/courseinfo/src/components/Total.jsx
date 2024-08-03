310e8f329ba798ea55717334f77bef64129e093c
lsteen-prog <linda.steenhuizen@gmail.com>
2024-08-03 15:26:57 +0200

    step 9

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,11 +1,10 @@
 const Total = ({ parts }) => {
-    console.log(parts)
 
     const total = parts.reduce(
         (sum, part) => sum + part['exercises'],
         0
     )
-    
+
     return (     
         <p>Number of exercises {total}</p>
     )
